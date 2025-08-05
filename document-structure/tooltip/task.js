cde1e752503804fa3b7c7c4e6f29164e0ee39b11
Daria <dariaxsh@yandex.ru>
2025-08-05 14:47:58 +0300

    commit tooltip

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,42 +1,26 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
+    
+    const tooltipContainer = document.createElement("div");
+    tooltipContainer.classList.add("tooltip");
+    document.body.appendChild(tooltipContainer);
+
     const tooltipElements = document.querySelectorAll(".has-tooltip");
 
     tooltipElements.forEach(function(el) {
         el.addEventListener("click", function(e) {
-            e.preventDefault(); // предотвращаем стандартный переход по ссылке
+            e.preventDefault(); 
+            const isCurrentLinkSameAsTooltip = this.title === tooltipContainer.textContent.trim();
 
-            // Проверяем, существует ли уже активная подсказка
-            const currentActiveTooltip = document.querySelector(".tooltip_active");
-            if (currentActiveTooltip) {
-                currentActiveTooltip.classList.remove("tooltip_active"); // прячем предыдущую подсказку
+            if (isCurrentLinkSameAsTooltip) {
+                tooltipContainer.classList.toggle("tooltip_active");
+            } else {
+                tooltipContainer.textContent = this.title;
+                tooltipContainer.classList.add("tooltip_active");
+
+                const rect = this.getBoundingClientRect();
+                tooltipContainer.style.left = `${rect.left + window.scrollX}px`;
+                tooltipContainer.style.top = `${rect.bottom + window.scrollY}px`;
             }
-
-            // Создаем блок подсказки
-            const tooltip = document.createElement("div");
-            tooltip.classList.add("tooltip");
-            tooltip.textContent = this.title; // используем текст из атрибута title
-
-            // Позиционируем подсказку близко к элементу
-            const rect = this.getBoundingClientRect();
-            tooltip.style.left = `${rect.left + window.scrollX}px`;
-            tooltip.style.top = `${rect.bottom + window.scrollY}px`;
-
-            // Добавляем подсказку в документ
-            document.body.appendChild(tooltip);
-
-            // Делаем видимую подсказку
-            tooltip.classList.add("tooltip_active");
-
-            // Удаляем подсказку при следующем клике
-            this.addEventListener("click", removeTooltip);
         });
     });
-
-    // Удаляет подсказку при втором клике
-    function removeTooltip() {
-        const currentTooltip = document.querySelector(".tooltip_active");
-        if (currentTooltip) {
-            currentTooltip.parentNode.removeChild(currentTooltip);
-        }
-    }
 });
