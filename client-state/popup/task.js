3a0f8d8a98ddde3cfd5c1308e69803a2bd6b071b
Daria <dariaxsh@yandex.ru>
2025-08-16 22:34:46 +0300

    second commit popup

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -2,13 +2,26 @@ document.addEventListener('DOMContentLoaded', function() {
     const modal = document.getElementById('subscribe-modal');
     const closeButton = document.querySelector('.modal__close');
 
-    
-    if (!localStorage.getItem('modalClosed')) {
+
+    if (!readCookie('modalClosed')) {
         modal.classList.add('modal_active');
     }
 
     closeButton.addEventListener('click', function() {
         modal.classList.remove('modal_active');
-        localStorage.setItem('modalClosed', true);
+        createCookie('modalClosed', true, 365); 
     });
 });
+
+function readCookie(name) {
+    const value = '; ' + document.cookie;
+    const parts = value.split('; ' + name + '=');
+    if (parts.length === 2) return parts.pop().split(';').shift();
+}
+
+function createCookie(name, value, days) {
+    const date = new Date();
+    date.setTime(date.getTime() + (days*24*60*60*1000));
+    const expires = "expires="+date.toGMTString();
+    document.cookie = name+"="+value+"; "+expires+"; path=/";
+}
